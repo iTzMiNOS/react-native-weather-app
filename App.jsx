0e925c9d2ae23df8e56e0568b011ac9083d5a0b2
iTzMiNOS <amirerf1998@gmail.com>
2023-08-26 15:24:40 +0300

    modified:   .gitignore 	modified:   App.jsx 	modified:   babel.config.js 	modified:   package-lock.json 	modified:   package.json

--- App.jsx
+++ App.jsx
@@ -1,13 +1,50 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import Tabs from './src/components/Tabs';
+import { ActivityIndicator, StyleSheet, View } from 'react-native';
+import * as Location from 'expo-location';
+import {API} from '@env';
 
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 const App = () => {
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [location, setLocation] = React.useState(null);
+  const [error, setError] = React.useState(null);
+
+  React.useEffect(() => {
+    (async() => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted'){
+        setError('Permission to access location was denied');
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size={'large'} color={'blue'} />
+      </View>
+    );
+  }
+
+
   return (
     <NavigationContainer>
       <Tabs />
     </NavigationContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    flex: 1,
+  },
+});
 
 export default App;
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
+/* eslint-disable prettier/prettier */
 module.exports = {
   presets: ['babel-preset-expo'],
+  plugins: [
+    ['module:react-native-dotenv', {
+      'envName': 'APP_ENV',
+      'moduleName': '@env',
+      'path': '.env',
+      'blocklist': null,
+      'allowlist': null,
+      'safe': false,
+      'allowUndefined': true,
+      'verbose': false,
+    }],
+  ],
 };
